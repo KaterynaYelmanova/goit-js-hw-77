6b211689a96b196cf0d576063058fd40cd73794d
KaterynaYelmanova <148703875+KaterynaYelmanova@users.noreply.github.com>
2024-02-24 12:01:35 +0200

    js7start

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,12 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const list = document.getElementsByClassName("item");
+//console.log(list);
+console.log(`Number of categories: ${list.length}`);
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const titles = document.querySelectorAll("h2");
+console.log(titles);
+titles.forEach(function (title) {
+  const titleName = title.textContent;
+  console.log(`Category: ${titleName}`);
+  const categoriesNames = title.parentElement.querySelectorAll("ul>li");
+  console.log(`Elements: ${categoriesNames.length}`);
+});
